137a0eb6f5f35d9528e05029af3942510ed7cead
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-06 23:05:52 +0530

    Added types

---
+++ src/context/types.js
@@ -0,0 +1,28 @@
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const AUTH_ERROR = "AUTH_ERROR";
+
+export const USER_LOADED = "USER_LOADED";
+
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+// Expense
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const UPDATE_EXPENSE = 'UPDATE_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+
+// Current
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+
+//Filter
+export const FILTER_CONTACTS = 'FILTER_CONTACTS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+
+// Alert
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
