11affb4183643be4fb6b3431f6f8a5f39b26161a
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-10 19:49:33 +0530

    Added Category Reducer

---
+++ src/context/category/categoryReducer.js
@@ -0,0 +1,75 @@
+import {
+  ADD_CATEGORY,
+  UPDATE_CATEGORY,
+  DELETE_CATEGORY,
+  GET_CATEGORIES,
+  CLEAR_CATEGORIES,
+  CATEGORY_ERROR,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+} from "../types";
+
+function categoryReducer(state, action) {
+  switch (action.type) {
+    case ADD_CATEGORY:
+      return {
+        ...state,
+        categories: [...state.categories, action.payload],
+        loading: false,
+      };
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload,
+        loading: false,
+      };
+    case CLEAR_CATEGORIES:
+      return {
+        ...state,
+        categories: [],
+        current: null,
+        error: null,
+        loading: false,
+      };
+
+    case DELETE_CATEGORY:
+      return {
+        ...state,
+        categories: state.categories.filter(
+          (category) => category._id !== action.payload
+        ),
+        loading: false,
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+
+    case UPDATE_CATEGORY:
+      return {
+        ...state,
+        categories: state.categories.map((category) =>
+          category._id === action.payload._id ? action.payload : category
+        ),
+      };
+
+    case CATEGORY_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
+}
+
+export default categoryReducer;
