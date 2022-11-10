581c4004de3cc5e372ee1cdcda5400f544a7ee00
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-10 19:52:22 +0530

    Added Select Categories Component for

---
+++ src/components/category/CategoriesSelect.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const CategoriesSelect = ({expense, setExpense}) => {
+  const [options, setOptions] = useState([]);
+
+  const fecthData = async () => {
+    try {
+      const res = await axios.get("/category/getAll");
+      const results = [];
+
+      res.data.categories.forEach((d) =>
+        results.push({ value: d._id, key: d.categoryName })
+      );
+
+      setOptions([{ key: "Choose a category", value: "" }, ...results]);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
+  const onChange = (e) => {
+    setExpense({ ...expense, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="col-auto">
+      <div className="form-floating">
+        <select
+          className="form-select"
+          id="categoryId"
+          aria-label="Select Expense Category"
+          name="categoryId"
+          value={expense.categoryId}
+          onChange={onChange}
+        >
+          {options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.key}
+              </option>
+            );
+          })}
+        </select>
+        <label htmlFor="categoryId">Category</label>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesSelect;
