da8daa2c0f75cf1579cbe025f3390f87579c31a6
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-10 19:53:24 +0530

    added types for expenses and categories

--- src/context/types.js
+++ src/context/types.js
@@ -12,15 +12,18 @@ export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
 // Expense
 export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const GET_EXPENSES = 'GET_EXPENSES';
+export const CLEAR_EXPENSES = 'CLEAR_EXPENSES';
 export const UPDATE_EXPENSE = 'UPDATE_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const EXPENSE_ERROR = 'EXPENSE_ERROR';
 
 // Current
 export const SET_CURRENT = 'SET_CURRENT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 
 //Filter
-export const FILTER_CONTACTS = 'FILTER_CONTACTS';
+export const FILTER_EXPENSES = 'FILTER_EXPENSES';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 
 // Alert
@@ -29,3 +32,11 @@ export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 //set loading
 export const SET_LOADING = "SET_LOADING";
+
+//Category
+export const ADD_CATEGORY = "ADD_CATEGORY";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const DELETE_CATEGORY = "DELETE_CATEGORY";
+export const GET_CATEGORIES = "GET_CATEGORIES";
+export const CLEAR_CATEGORIES = "CLEAR_CATEGORIES";
+export const CATEGORY_ERROR = "CATEGORY_ERROR";
