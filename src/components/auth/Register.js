c8a11d73c020c6eab4e3f25f950046df5d7f768c
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-11 19:14:50 +0530

    Updated UI

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
@@ -69,87 +69,98 @@ const Register = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="text-center">Registration</h1>
-      <form className="p-4">
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            id="name"
-            name="name"
-            className="form-control"
-            type="text"
-            placeholder="Enter name"
-            onChange={onChange}
-            value={name}
-            required
-          />
-        </div>
+    <Fragment>
+<div className="container py-4 mt-4">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card bg-dark" style={{ borderRadius: "1rem" }}>
+              <div className="card-body p-5 text-center">
+                <div className="">
+                  <h2 className="fw-bold mb-2 text-uppercase text-white">
+                    Register
+                  </h2>
+                  <p className="text-white-50 mb-5 text-white">
+                    Please enter your Name, Email and Password to Register
+                  </p>
 
-        <div className="mb-3">
-          <label htmlFor="registerEmail" className="form-label">
-            Email
-          </label>
-          <input
-            id="registerEmail"
-            name="email"
-            className="form-control"
-            type="email"
-            placeholder="Enter email"
-            aria-describedby="emailHelp"
-            onChange={onChange}
-            value={email}
-            required
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+                  <div className="form-floating form-outline form-white mb-4">
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="form-control form-control-lg"
+                      placeholder="Enter Name"
+                      value={name}
+                      onChange={onChange}
+                    />
+                    <label className="form-label" htmlFor="name">
+                      Name
+                    </label>
+                  </div>
+
+                  <div className="form-floating form-outline form-white mb-4">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="form-control form-control-lg"
+                      placeholder="Enter Email"
+                      aria-describedby="emailHelp"
+                      value={email}
+                      onChange={onChange}
+                    />
+                    <label className="form-label" htmlFor="email">
+                      Email
+                    </label>
+                    <div id="emailHelp" className="form-text">
+                      We'll never share your email with anyone else.
+                    </div>
+                  </div>
+
+                  <div className="form-floating mb-4">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      className="form-control form-control-lg"
+                      placeholder="Enter Password"
+                      value={password}
+                      onChange={onChange}
+                    />
+                    <label className="form-label" htmlFor="password">
+                      Password
+                    </label>
+                  </div>
+
+                  <div className="form-floating mb-4">
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      className="form-control form-control-lg"
+                      placeholder="Enter Password"
+                      value={confirmPassword}
+                      onChange={onChange}
+                    />
+                    <label className="form-label" htmlFor="confirmPassword">
+                      Confirm Password
+                    </label>
+                  </div>
+
+                  <button
+                    className="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    Register | <i className="fa-solid fa-user-plus" />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="registerPassword" className="form-label">
-            Password
-          </label>
-          <input
-            id="registerPassword"
-            name="password"
-            className="form-control"
-            type="password"
-            placeholder="Password"
-            onChange={onChange}
-            value={password}
-            required
-            minLength={6}
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="confirmPassword" className="form-label">
-            Confirm Password
-          </label>
-          <input
-            id="confirmPassword"
-            name="confirmPassword"
-            className="form-control"
-            type="password"
-            placeholder="Re-enter password"
-            onChange={onChange}
-            value={confirmPassword}
-            required
-            minLength={6}
-          />
-        </div>
-        <button
-          className="btn my-btn-primary"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Register | <i className="fa-solid fa-user-plus" />
-        </button>
-      </form>
-    </div>
+      </div>
+    </Fragment>
   );
 };
 
