839edc98901beff14447b8fcdb83c775735363c3
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-11 19:20:20 +0530

    added container and flex

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,26 +1,29 @@
 import React, { Fragment, useContext } from "react";
 import { useEffect } from "react";
 import AuthContext from "../../context/auth/authContext";
+import ExpenseFilter from "../expenses/ExpenseFilter";
 import ExpenseForm from "../expenses/ExpenseForm";
 import Expenses from "../expenses/Expenses";
 import ExpenseSearch from "../expenses/ExpenseSearch";
 
 const Home = () => {
+  const { loadUser } = useContext(AuthContext);
 
-  const {loadUser} = useContext(AuthContext);
-
-  useEffect(()=>{
+  useEffect(() => {
     loadUser();
     // eslint-disable-next-line
-  },[])
+  }, []);
 
   return (
-    <Fragment>
+    <div className="container pt-5">
       <ExpenseForm />
-      <h2 className="mt-3 mx-3 text-primary">Expenses:</h2>
-      <ExpenseSearch />
+      <h2 className="mt-3 text-black">Expenses</h2>
+      <div className="d-flex justify-content-between">
+        <ExpenseSearch />
+        <ExpenseFilter />
+      </div>
       <Expenses />
-    </Fragment>
+    </div>
   );
 };
 
