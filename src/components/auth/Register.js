9d89350da4dde0cb736aae97ac947df1b50fd575
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-15 20:19:03 +0530

    form validation

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -51,8 +51,6 @@ const Register = () => {
       email,
       password,
     });
-
-    // clearField();
   };
 
   const isPasswordMatched = () => {
@@ -70,7 +68,7 @@ const Register = () => {
 
   return (
     <Fragment>
-<div className="container py-4 mt-4">
+      <div className="container py-4 mt-4">
         <div className="row d-flex justify-content-center align-items-center">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
             <div className="card bg-dark" style={{ borderRadius: "1rem" }}>
@@ -82,78 +80,85 @@ const Register = () => {
                   <p className="text-white-50 mb-5 text-white">
                     Please enter your Name, Email and Password to Register
                   </p>
-
-                  <div className="form-floating form-outline form-white mb-4">
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Name"
-                      value={name}
-                      onChange={onChange}
-                    />
-                    <label className="form-label" htmlFor="name">
-                      Name
-                    </label>
-                  </div>
-
-                  <div className="form-floating form-outline form-white mb-4">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Email"
-                      aria-describedby="emailHelp"
-                      value={email}
-                      onChange={onChange}
-                    />
-                    <label className="form-label" htmlFor="email">
-                      Email
-                    </label>
-                    <div id="emailHelp" className="form-text">
-                      We'll never share your email with anyone else.
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-floating form-outline form-white mb-4">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Name"
+                        value={name}
+                        onChange={onChange}
+                        required
+                        minLength={3}
+                      />
+                      <label className="form-label" htmlFor="name">
+                        Name
+                      </label>
                     </div>
-                  </div>
 
-                  <div className="form-floating mb-4">
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Password"
-                      value={password}
-                      onChange={onChange}
-                    />
-                    <label className="form-label" htmlFor="password">
-                      Password
-                    </label>
-                  </div>
+                    <div className="form-floating form-outline form-white mb-4">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Email"
+                        aria-describedby="emailHelp"
+                        value={email}
+                        onChange={onChange}
+                        required
+                      />
+                      <label className="form-label" htmlFor="email">
+                        Email
+                      </label>
+                      <div id="emailHelp" className="form-text">
+                        We'll never share your email with anyone else.
+                      </div>
+                    </div>
 
-                  <div className="form-floating mb-4">
-                    <input
-                      type="password"
-                      id="confirmPassword"
-                      name="confirmPassword"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Password"
-                      value={confirmPassword}
-                      onChange={onChange}
-                    />
-                    <label className="form-label" htmlFor="confirmPassword">
-                      Confirm Password
-                    </label>
-                  </div>
+                    <div className="form-floating mb-4">
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Password"
+                        value={password}
+                        onChange={onChange}
+                        required
+                        minLength={6}
+                      />
+                      <label className="form-label" htmlFor="password">
+                        Password
+                      </label>
+                    </div>
 
-                  <button
-                    className="btn btn-outline-light btn-lg px-5"
-                    type="submit"
-                    onClick={(e) => handleSubmit(e)}
-                  >
-                    Register | <i className="fa-solid fa-user-plus" />
-                  </button>
+                    <div className="form-floating mb-4">
+                      <input
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Password"
+                        value={confirmPassword}
+                        onChange={onChange}
+                        required
+                        minLength={6}
+                      />
+                      <label className="form-label" htmlFor="confirmPassword">
+                        Confirm Password
+                      </label>
+                    </div>
+
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      type="submit"
+                    >
+                      Register | <i className="fa-solid fa-user-plus" />
+                    </button>
+                  </form>
                 </div>
               </div>
             </div>
